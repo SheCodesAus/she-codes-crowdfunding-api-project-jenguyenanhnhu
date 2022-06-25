ef7b833c4b1bcb90ad72994ebc34e4a2d46e200a
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-06-25 12:26:58 +1000

    Created Project and Pledge views, models, urls and serializers.
    Completed in class.
    Followed class example.
    Still need to customise models and serializers.

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.TextField()
+    image = models.FileField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.CharField(max_length=200)
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey('Project',on_delete=models.CASCADE,related_name='pledges')
+    supporter = models.CharField(max_length=200)
