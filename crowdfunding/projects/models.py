fb5fe0b87f2428ca6fa597102488fb6f0ece320f
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-06-28 20:28:25 +1000

    Create Users and Permissions functionality
    Create User Apps.
    Added Update function if user is project owner.

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
@@ -7,11 +8,27 @@ class Project(models.Model):
     image = models.FileField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
+PLEDGE_TYPES = [
+    ('$', 'Money'),
+    ('T', 'Time'),
+    ('A', 'Advice'),
+    ('R', 'Resources'),
+    ('O', 'Other'),
+]
 class Pledge(models.Model):
+    type = models.CharField(max_length=20, choices=PLEDGE_TYPES)
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     project = models.ForeignKey('Project',on_delete=models.CASCADE,related_name='pledges')
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from .models import Project, Pledge
+from .models import Project, Pledge, PLEDGE_TYPES
 
 class PledgeSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
+    type = serializers.ChoiceField(choices=PLEDGE_TYPES)
     amount = serializers.IntegerField()
     comment = serializers.CharField(max_length=200)
     anonymous = serializers.BooleanField()
@@ -20,11 +21,23 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.FileField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
-    owner = serializers.CharField(max_length=200)
+    owner = serializers.ReadOnlyField(source='owner.id')
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
 
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description',instance.description)
+        instance.goal = validated_data.get('goal', instance.goal)
+        instance.image = validated_data.get('image', instance.image)
+        instance.is_open = validated_data.get('is_open',instance.is_open)
+        instance.date_created = validated_data.get('date_created',instance.date_created)
+        instance.owner = validated_data.get('owner', instance.owner)
+        instance.save()
+        return instance
+
 
