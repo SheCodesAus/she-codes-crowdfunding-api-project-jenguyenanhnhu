9815cdfffcb9d2d424da1458182bf1abcb6a9cc6
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-07-09 11:57:50 +1000

    Update serializers.py

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,7 +5,7 @@ class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=20)
     first_name = serializers.CharField(max_length=50)
-    surname = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
     email = serializers.CharField(max_length=200)
     
 def create(self, validated_data):
