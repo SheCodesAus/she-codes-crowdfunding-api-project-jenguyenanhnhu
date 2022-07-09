b6032abfad5b40fdcaa2464dd6fdcabb69ccfd25
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-07-09 13:13:58 +1000

    Update views.py

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -52,6 +52,6 @@ class CustomUserDetail(APIView):
         if serializer.is_valid():
             
             serializer.save()
-            print(serializer.data)
+            # print(serializer.data)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
