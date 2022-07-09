5b027ae9c779681e856acf23abfff26cb3c6ef57
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-07-09 13:04:08 +1000

    Update views.py

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -34,13 +34,22 @@ class CustomUserDetail(APIView):
             return Response(serializer.data)
         return Response({"Oops! You're trying to look at another user's details. Please go to your user profile."}, status=status.HTTP_401_UNAUTHORIZED)
 
+    # def put(self, request, pk):
+    #     if request.user == self.get_object(pk):
+    #         user = self.get_object(pk)
+    #         data = request.data
+    #         serializer = CustomUserDetailSerializer(instance=user, data=data, partial=True)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    #         return Response(serializer.errors, {"Oops! You're changing your user information to something strange. Give it another go."}, status=status.HTTP_400_BAD_REQUEST)
+    #     return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
     def put(self, request, pk):
-        if request.user == self.get_object(pk):
-            user = self.get_object(pk)
-            data = request.data
-            serializer = CustomUserDetailSerializer(instance=user, data=data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-            return Response(serializer.errors, {"Oops! You're changing your user information to something strange. Give it another go."}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+        user = self.get_object(pk)
+        data = request.data
+        serializer = CustomUserSerializer(instance=user, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
