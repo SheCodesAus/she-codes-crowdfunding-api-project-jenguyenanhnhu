509ffb6d9899fcd3a0f9d922b47800a4c5d135e0
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-08-27 14:20:44 +1000

    Update serializers.py

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -3,29 +3,29 @@ from .models import CustomUser
 
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
-    username = serializers.CharField(max_length=20)
+    email=serializers.EmailField()
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
-    email = serializers.EmailField()
     password = serializers.CharField()
+    bio = serializers.CharField(max_length=500)
     
     def create(self, validated_data):
         user = CustomUser(
-            username=validated_data['username'],
+            email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            email=validated_data['email'],
+            bio=validated_data['bio'],
         )
         user.set_password(validated_data['password'])
         user.save()
         return user
 
     def update(self, instance, validated_data):
-        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
         instance.first_name = validated_data.get('first_name',instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
         instance.password = instance.password
+        instance.bio = instance.bio.get('bio', instance.bio)
         instance.save()
         return instance
 
